3358c988fd213f24260f5510e103b4d0c8ccfd04
Diego Bautista López <diego.bautistamlp@gmail.com>
2024-05-17 20:30:01 -0500

    style: remove icons from sidebar

--- getsemani-assistance-frontend/src/app/app.component.ts
+++ getsemani-assistance-frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/component/navbar/navbar.component';
 import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from './shared/component/sidebar/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [SidebarComponent,RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
