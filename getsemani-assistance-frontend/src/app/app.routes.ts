2359ecbd64500d829606e394bf9377259a21835b
Sair Marquez <sair2003.hotmail.com@gmail.com>
2024-05-20 17:54:55 -0500

    change(frontend): sidebar

--- getsemani-assistance-frontend/src/app/app.routes.ts
+++ getsemani-assistance-frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { StudentComponent } from './page/student/student.component';
 import { AssistanceComponent } from './page/assistance/assistance.component';
-import { SidebarComponent } from './shared/component/sidebar/sidebar/sidebar.component';
+import { LoginComponent } from './page/login/login.component';
 
 export const routes: Routes = [
   {path: 'estudiantes', component: StudentComponent},
   {path: 'asistencia', component: AssistanceComponent},
-  {path: 'sidebar', component: SidebarComponent}
+  {path: 'login', component: LoginComponent}
 ];
